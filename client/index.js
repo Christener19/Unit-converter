4f9c01d12b38872db69dd8d39a3d8b86d2f253be
Christener19 <christenerleigh7070@gmail.com>
2024-03-15 13:57:07 +0000

    all conversions done

--- client/index.js
+++ client/index.js
@@ -1,26 +1,28 @@
-/*
-CONVERSION NUMBERS:   
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
 let input = document.querySelector(".number-to-convert")
 let convert = document.querySelector(".conversion-btn")
 let lengths = document.querySelector(".length")
 let liters = document.querySelector(".liters")
 let masss = document.querySelector(".mass")
-let meter;
-let liter;
-let kilogram;
-let pound;
-let gallen;
-let feet;
+// let meter;
+// let liter;
+// let kilogram;
+// let pound;
+// let gallon;
+// let feet;
 
 convert.addEventListener("click", function() {
-    feet = input.value * 3.281
-    meter = input.value * 0.3048
+ let feet = input.value * 3.281
+  let meter = input.value * 0.3048
     lengths.textContent = 
     ` ${input.value} meters = ${feet} feet |
      ${input.value} feet = ${meter} meters`
+    let liter = input.value * 3.785
+   let  gallon = input.value * 0.264
+    liters.textContent =
+    `${input.value} liters = ${gallon} gallons | ${input.value} gallons = ${liter} liters`
+    let kilogram = input.value * 2.204
+    let pound = input.value * 0.4535
+    masss.textContent =
+    `${input.value} kilos = ${kilogram} pounds | ${input.value} pounds = ${pound} kilos`
 })
 
