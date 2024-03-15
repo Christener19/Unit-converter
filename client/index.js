f79add00aca5aafbe3cc93233a9d67a8b6fe99a6
Christener19 <christenerleigh7070@gmail.com>
2024-03-15 13:01:20 +0000

    html and css done

---
+++ client/index.js
@@ -0,0 +1,11 @@
+/*
+
+CONVERSION NUMBERS:   
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+
+
+*/
+let conversionNumber = document.querySelector(".number-to-convert")
+let convert = document.querySelector(".conversion-btn")
